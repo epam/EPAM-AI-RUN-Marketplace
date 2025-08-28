38d2db615eb21e4277aaea51793743218560da81
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2025-08-28 18:43:18 +0200

    Add aitestmate-redis chart

---
+++ create-secrets.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+NAMESPACE="$1"
+
+namespace-p() {
+  local namespace="$1"
+
+  kubectl get namespace "$namespace" -o name &> /dev/null
+}
+
+secret-p() {
+  local namespace="$1"
+  local secret_name="$2"
+
+  kubectl get secret "$secret_name" -o name -n "$namespace" &> /dev/null
+}
+
+create-secret() {
+    kubectl --namespace "$NAMESPACE" create secret generic --type opaque "$@"
+}
+
+
+if ! namespace-p "$NAMESPACE"; then
+    echo "'$NAMESPACE' does not exist"
+    exit 1
+fi
+
+create-secret aitestmate-redis-secret \
+              --from-literal="REDIS_PASSWORD=$(pwgen 24 1)"
