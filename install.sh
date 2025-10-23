6f6b87013a240a3bd7d23a5599067a8c3093140b
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2025-10-23 16:35:11 +0200

    Allow image.repository and image.tag overrides from install script

--- install.sh
+++ install.sh
@@ -1,21 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env  bash
 
 if [[ -z "$1" ]]; then
-  echo "Usage: $0 <namespace>"
+    echo "Usage: $0 <namespace> [container-registry] [version]"
+    echo "  namespace: kubernetes namespace, e.g. aitestmate"
+    echo "  container-registry: registry with aitestmate images, e.g. 000000000000.dkr.ecr.us-east-1.amazonaws.com"
+    echo "  version: override version, e.g. 2.2.1-aws"
   exit 1
 fi
 
 NAMESPACE="$1"
+CONTAINER_REGISTRY="$2"
+IMAGE_TAG="$3"
 
-helm upgrade "$NAMESPACE-redis"         charts/aitestmate-redis         --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-redis/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-rabbitmq"      charts/aitestmate-rabbitmq      --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-rabbitmq/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-elasticsearch" charts/aitestmate-elasticsearch --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-elasticsearch/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-kibana"        charts/aitestmate-kibana        --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-kibana/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-embeddings"    charts/aitestmate-embeddings    --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-embeddings/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-migrator"      charts/aitestmate-migrator      --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-migrator/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-beat"          charts/aitestmate-beat          --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-beat/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-flower"        charts/aitestmate-flower        --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-flower/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-sysworker"     charts/aitestmate-sysworker     --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-sysworker/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-worker"        charts/aitestmate-worker        --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-worker/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-api"           charts/aitestmate-api           --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-api/examples/aws/values.yaml
-helm upgrade "$NAMESPACE-nginx"         charts/aitestmate-nginx         --install --namespace "$NAMESPACE" --create-namespace -f charts/aitestmate-nginx/examples/aws/values.yaml
+function helm-upgrade() {
+    local name="$1"; shift
+
+    echo helm upgrade "$NAMESPACE-$name" "charts/aitestmate-$name" \
+         --install --namespace "$NAMESPACE" --create-namespace \
+         ${CONTAINER_REGISTRY:+--set image.repository=$CONTAINER_REGISTRY/aitestmate/$name} \
+         ${IMAGE_TAG:+--set image.tag=$IMAGE_TAG} \
+         "$@"
+}
+
+helm-upgrade redis         -f charts/aitestmate-redis/examples/aws/values.yaml
+helm-upgrade rabbitmq      -f charts/aitestmate-rabbitmq/examples/aws/values.yaml
+helm-upgrade elasticsearch -f charts/aitestmate-elasticsearch/examples/aws/values.yaml
+helm-upgrade kibana        -f charts/aitestmate-kibana/examples/aws/values.yaml
+helm-upgrade embeddings    -f charts/aitestmate-embeddings/examples/aws/values.yaml
+helm-upgrade migrator      -f charts/aitestmate-migrator/examples/aws/values.yaml
+helm-upgrade beat          -f charts/aitestmate-beat/examples/aws/values.yaml
+helm-upgrade flower        -f charts/aitestmate-flower/examples/aws/values.yaml
+helm-upgrade sysworker     -f charts/aitestmate-sysworker/examples/aws/values.yaml
+helm-upgrade worker        -f charts/aitestmate-worker/examples/aws/values.yaml
+helm-upgrade api           -f charts/aitestmate-api/examples/aws/values.yaml
+helm-upgrade nginx         -f charts/aitestmate-nginx/examples/aws/values.yaml
