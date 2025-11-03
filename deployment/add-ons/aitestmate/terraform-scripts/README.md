03fa86c40f56975f78f5861e5df77b46bdbf2cb1
Yauhen <jsirex@gmail.com>
2025-11-03 14:26:49 +0100

    AI TestMate Terraform Scripts

---
+++ deployment/add-ons/aitestmate/terraform-scripts/README.md
@@ -0,0 +1,29 @@
+# AI/Run TestMate AWS Terraform Module
+
+## Requirements
+
+- Terraform version: `>= 1.5.7`
+
+## Notes
+
+Ensure you configure a proper backend for state management if required for your environment.
+
+## Customizing Default Variables
+
+Default variables can be customized by assigning values in a `terraform.tfvars` file.
+Also variables could be set via `TF_VAR_<variable>=<value>` environment variable.
+
+## Usage
+
+1. Initialize Terraform:
+   ```bash
+   terraform init
+   ```
+2. Validate and review the plan:
+   ```bash
+   terraform plan
+   ```
+3. Apply the configuration:
+   ```bash
+   terraform apply
+   ```
---
+++ deployment/add-ons/aitestmate/terraform-scripts/outputs.tf
@@ -0,0 +1,28 @@
+output "account_id" {
+  description = "Detected account id"
+  value       = data.aws_caller_identity.current.account_id
+}
+
+output "worker_role_arn" {
+  description = "IAM role arn for AI/Run TestMate worker pods"
+  value       = module.worker_role.arn
+}
+
+output "sysworker_role_arn" {
+  description = "IAM role arn for AI/Run TestMate sysworker pod"
+  value       = module.sysworker_role.arn
+}
+output "api_role_arn" {
+  description = "IAM role arn for AI/Run TestMate api pods"
+  value       = module.api_role.arn
+}
+
+output "kms_default_key_id" {
+  description = "KMS Default key id for AI/Run TestMate"
+  value       = module.default_key.key_id
+}
+
+output "kms_codemie_key_id" {
+  description = "KMS Codemie integration key id for AI/Run TestMate"
+  value       = module.codemie_key.key_id
+}
