6717fc827798c03f3d6475e0e0e974cbedaf4dd7
Yauhen <jsirex@gmail.com>
2025-11-03 14:27:22 +0100

    AI TestMate Helm Charts

---
+++ deployment/add-ons/aitestmate/helm-scripts/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env  bash
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <namespace> [container-registry] [version]"
+    echo "  namespace: kubernetes namespace, e.g. aitestmate"
+    echo "  container-registry: registry with aitestmate images, e.g. 000000000000.dkr.ecr.us-east-1.amazonaws.com"
+    echo "  version: override version, e.g. 2.2.1-aws"
+  exit 1
+fi
+
+NAMESPACE="$1"
+CONTAINER_REGISTRY="$2"
+IMAGE_TAG="$3"
+
+function helm-upgrade() {
+    local name="$1"; shift
+
+    helm upgrade "$NAMESPACE-$name" "charts/aitestmate-$name" \
+         --install --namespace "$NAMESPACE" --create-namespace \
+         ${CONTAINER_REGISTRY:+--set image.repository=$CONTAINER_REGISTRY/aitestmate/$name} \
+         ${IMAGE_TAG:+--set image.tag=$IMAGE_TAG} \
+         "$@"
+}
+
+helm-upgrade redis         -f charts/aitestmate-redis/examples/aws/values.yaml
+helm-upgrade rabbitmq      -f charts/aitestmate-rabbitmq/examples/aws/values.yaml
+helm-upgrade elasticsearch -f charts/aitestmate-elasticsearch/examples/aws/values.yaml
+helm-upgrade kibana        -f charts/aitestmate-kibana/examples/aws/values.yaml
+helm-upgrade embeddings    -f charts/aitestmate-embeddings/examples/aws/values.yaml
+helm-upgrade migrator      -f charts/aitestmate-migrator/examples/aws/values.yaml
+helm-upgrade beat          -f charts/aitestmate-beat/examples/aws/values.yaml
+helm-upgrade flower        -f charts/aitestmate-flower/examples/aws/values.yaml
+helm-upgrade sysworker     -f charts/aitestmate-sysworker/examples/aws/values.yaml
+helm-upgrade worker        -f charts/aitestmate-worker/examples/aws/values.yaml
+helm-upgrade api           -f charts/aitestmate-api/examples/aws/values.yaml
+helm-upgrade nginx         -f charts/aitestmate-nginx/examples/aws/values.yaml
