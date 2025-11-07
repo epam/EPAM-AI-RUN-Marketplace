b64af6a8d3a5f67b4631d5c11ebf2ca49262dca1
Oleksandr Makiiov <Oleksandr_Makiiov@epam.com>
2025-11-07 13:28:17 +0100

    cp for Windows/GitBash support - 2

--- deployment/add-ons/aice/helm-scripts/install.sh
+++ deployment/add-ons/aice/helm-scripts/install.sh
@@ -362,11 +362,11 @@ deploy_neo4j() {
     kubectl cp ./dozerdb-plugin-5.26.3.0.jar aice-neo4j-0:/plugins -c neo4j -n "$namespace"
     kubectl exec aice-neo4j-0 -c neo4j -n "$namespace" -- chown neo4j:neo4j /plugins/dozerdb-plugin-5.26.3.0.jar
 
-    cp $SCRIPT_DIR/artifacts/neo4j/plugins/apoc-5.26.3-core.jar
+    cp $SCRIPT_DIR/artifacts/neo4j/plugins/apoc-5.26.3-core.jar .
     kubectl cp ./apoc-5.26.3-core.jar aice-neo4j-0:/plugins -c neo4j -n "$namespace"
     kubectl exec aice-neo4j-0 -c neo4j -n "$namespace" -- chown neo4j:neo4j /plugins/apoc-5.26.3-core.jar
 
-    cp $SCRIPT_DIR/artifacts/neo4j/plugins/neo4j-graph-data-science-2.13.4.jar
+    cp $SCRIPT_DIR/artifacts/neo4j/plugins/neo4j-graph-data-science-2.13.4.jar .
     kubectl cp ./neo4j-graph-data-science-2.13.4.jar aice-neo4j-0:/plugins -c neo4j -n "$namespace"
     kubectl exec aice-neo4j-0 -c neo4j -n "$namespace" -- chown neo4j:neo4j /plugins/neo4j-graph-data-science-2.13.4.jar
 
